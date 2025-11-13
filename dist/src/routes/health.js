bbe2752381bafe2debd32dba91942f3644a8a6b1
David Rafael Dominguez Quezada <127813089+dodamivid@users.noreply.github.com>
2025-11-13 08:47:04 -0600

    Revert "Agregar módulo de Dashboard y herramientas de estilo de código"

--- dist/src/routes/health.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-// Lightweight liveness probe that the integration ticket expects
-router.get("/", (_req, res) => {
-    res.status(200).json({ status: "ok" });
-});
-exports.default = router;
